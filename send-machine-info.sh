cbefcb35a6c99a72ed01a7188618c39b29f7e539
michaeljymsgutierrez <leeez711@gmail.com>
2021-04-25 00:05:14 +0800

    Updated send machine info script

--- send-machine-info.sh
+++ send-machine-info.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
 
-echo -e "HOSTNAME   : $HOSTNAME\nUSERNAME   : $USER\nEXTERNAL IP: $(curl ifconfig.me)\nINTERNAL IP : $(hostname -I)" > machine.txt && node index.js
+# Configure the path of this script to cron
+
+cd $HOME/Projects/machine-email-local-info
+
+echo -e "HOSTNAME: $HOSTNAME\nUSERNAME: $(whoami)\nEXTERNAL IP: $(curl ifconfig.me)\nINTERNAL IP: $(hostname -I | awk '{print $1}')\nBATTERY: $(upower -i $(upower -e | grep '/battery') | grep --color=never -E percentage|xargs|cut -d' ' -f2|sed s/%//)%" > machine.txt
+
+$HOME/.nvm/versions/node/v10.22.1/bin/node index.js
